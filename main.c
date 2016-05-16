cf233477c24b92fa9d6148cc024e8e82fcd92867
Georg Rokita <rokita.georg@freenet.de>
2016-05-16 20:01:01 +0300

    validRFID function and add setTrain (timer function)

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 int RFIDResetPin = 0;
+bool validRFID = false;
+bool timerFlag = false;
+int redLed = 5;
+int greenLed = 6;
 
 void setup(){
     Serial.begin(9600); // the USB Serail communication between the PC and Photon 
@@ -7,29 +11,34 @@ void setup(){
     pinMode(RFIDResetPin, OUTPUT);
     digitalWrite(RFIDResetPin, HIGH);
 
+    // set up status Leds if the IoT Project is counting the pull ups
+    pinMode(redLed, OUTPUT);
+    pinMode(greenLed, OUTPUT);
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
 }
  
 void loop(){
 
-  //char tagString[13];
-  char* tagString;
-  //int index = 0;
-  //boolean reading = false;
+  char tagString[13];
+  
+  getRFIDTagName(tagString);
+  
+  validRFID = checkTag(tagString); //Check if it is a match
 
-  tagString = getRFIDTagName();
-   
+  // turn on green LED if valid RFID
+  setTrainLed(validRFID);
 
-  checkTag(tagString); //Check if it is a match
   clearTag(tagString); //Clear the char of all value
   resetReader(); //eset the RFID reader
 }
 
 
-char* getRFIDTagName(){
+void getRFIDTagName(char* tagStr){
     while(Serial1.available() > 0){
         int index = 0;
         boolean reading = false;
-        char tagStr[13];
+        //char tagStr[13];
         
         int readByte = Serial1.read(); //read next available byte
     
@@ -50,32 +59,59 @@ char* getRFIDTagName(){
             Serial.println("********************");
             
             //return the RFID tag value
-            return tagStr;
+            return;
         }
     }
 }
 
-void checkTag(char tag[]){
-///////////////////////////////////
-//Check the read tag against known tags
-/////////////////////////////////////
-    Serial.println("*******Check Tag***********");
-    Serial.println(tag); //read out any unknown tag
+void setTrainLed(bool trainValid) {
+    
+        Serial.print('timerFlag');
+        Serial.println(timerFlag);
 
-    if(strlen(tag) == 0) return; //empty, no need to contunue
-  
-    Serial.println(tag); //read out any unknown tag
-    return;
+
+      if ( trainValid == false && timerFlag == false){
+        
+            digitalWrite(redLed, HIGH);
+            digitalWrite(greenLed, HIGH); // needs to be Low
+      
+      }else{
+            digitalWrite(redLed, LOW);
+            digitalWrite(greenLed, HIGH);
+            timerFlag = true;
+      }
+      return;
+}
+
+bool checkTag(char tag[]){
+    ///////////////////////////////////
+    //Check the read tag against known tags
+    /////////////////////////////////////
+    
+        // Tags in the database
+        char bigWhiteTag[13] = "23006E7FC7F5";
+        //Serial.println("******* Check Tag ***********");
+        //Serial.println(tag); //read out any unknown tag
+    
+        if(strlen(tag) == 0) return false; //empty, no need to contunue
+      
+      // check which tag it is
+        if(tag == bigWhiteTag){
+          return true;
+        }else{
+          Serial.println(tag); //read out any unknown tag
+        }
+        return false;
 }
 
 void resetReader(){
-///////////////////////////////////
-//Reset the RFID reader to read again.
-///////////////////////////////////
-
-    digitalWrite(RFIDResetPin, LOW);
-  digitalWrite(RFIDResetPin, HIGH);
-  delay(250);
+    ///////////////////////////////////
+    //Reset the RFID reader to read again.
+    ///////////////////////////////////
+    
+      digitalWrite(RFIDResetPin, LOW);
+      digitalWrite(RFIDResetPin, HIGH);
+      delay(250);
 }
 
 void clearTag(char one[]){
